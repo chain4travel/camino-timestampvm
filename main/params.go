1ab06092161bbf98ea9c9478d6cbe1a3ee1f3529
Paweł Nowosielski <pawel.nowosielski@golem.network>
2022-07-18 14:11:59 +0200

    port timestampvm example to c4t

--- main/params.go
+++ main/params.go
@@ -1,4 +1,15 @@
-// (c) 2019-2020, Ava Labs, Inc. All rights reserved.
+// Copyright (C) 2022, Chain4Travel AG. All rights reserved.
+//
+// This file is a derived work, based on ava-labs code whose
+// original notices appear below.
+//
+// It is distributed under the same license conditions as the
+// original code from which it is derived.
+//
+// Much love to the original authors for their work.
+// **********************************************************
+
+// (c) 2021, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
 package main
@@ -6,7 +17,7 @@ package main
 import (
 	"flag"
 
-	"github.com/ava-labs/timestampvm/timestampvm"
+	"github.com/chain4travel/camino-timestampvm/timestampvm"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
